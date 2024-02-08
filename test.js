acae3c8ea256b072832c88c66dc68c9a677d4a8e
Mark Yousef <mark.yousef@partage.club>
2024-02-08 14:31:01 -0500

    testpush1

--- test.js
+++ test.js
@@ -21,15 +21,25 @@ async function runTest() {
   try {
     const el12 = await driver.$("xpath://android.widget.Button[@content-desc=\"Enter URL manually\"]/android.view.ViewGroup");
     await el12.click();
+    await wait(5000);
+
     const el13 = await driver.$("class name:android.widget.EditText");
     await el13.click();
+    await wait(5000);
+
     await el13.addValue("https://u.expo.dev/update/21b1cdb1-1af9-4ccb-a27c-d26825467f56");
+    await wait(5000);
+
     await driver.executeScript("mobile: pressKey", [{ "keycode": 4 }]);
     const el14 = await driver.$("accessibility id:Connect");
+    await wait(5000);
+
     await el14.click();
     await wait(5000);
     const el3 = await driver.$("xpath://android.widget.FrameLayout[@resource-id=\"club.partage.mobile.development:id/bottom_sheet\"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button/android.widget.ImageView");
     await el3.click();
+    await wait(5000);
+
     console.log("reussi");
   } 
   catch (error) {
