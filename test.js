d27a69177d4913470e6868e89e1fc09e4478360d
MarkYousef1 <155985419+MarkYousef1@users.noreply.github.com>
2024-02-09 11:28:29 -0500

    Update test.js

--- test.js
+++ test.js
@@ -36,6 +36,7 @@ async function runTest() {
       await driver.touchAction({
         action: 'tap', x: 1027, y: 1043
       });
+      console.log("pas reussi");
   } 
   catch (error) {
     console.error(error.message);
