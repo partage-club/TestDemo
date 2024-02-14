b485dfe2bd0565018b526f9cf6c5f23037008d17
Mark Yousef <mark.yousef@partage.club>
2024-02-14 11:32:48 -0500

    upd

--- test.js
+++ test.js
@@ -35,6 +35,18 @@ async function runTest() {
     await but.click();
   }
 
+  async function waitForElementDisplayed(xpath) {
+    let element = await driver.$(xpath);
+
+    while (!(await element.isDisplayed())) {
+        console.log(`Element with XPath ${xpath} is not displayed yet. Retrying...`);
+        // Optionally, you can introduce a small delay before retrying
+        await driver.pause(500);
+    }
+
+    return element;
+}
+
   //Cas de test 1
   try {
     await waitForButton("xpath://android.widget.Button[@content-desc=\"Enter URL manually\"]/android.view.ViewGroup");
@@ -52,13 +64,13 @@ async function runTest() {
     //const screenshotBefore = await driver.takeScreenshot();
     //fs.writeFileSync('screenshot_before.png', screenshotBefore, 'base64');
 
-    await driver.waitUntil(async () => {
-      const el1 = await driver.$("xpath://android.widget.FrameLayout[@resource-id=\"club.partage.mobile.development:id/bottom_sheet\"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button/android.widget.ImageView");
-      return await el1.isDisplayed();
-  }, { timeout: 99999, timeoutMsg: 'Button did not appear within 5000ms' });
-    const el1 = await driver.$("xpath://android.widget.FrameLayout[@resource-id=\"club.partage.mobile.development:id/bottom_sheet\"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button/android.widget.ImageView");
-    el1.click();
-
+ 
+  
+  // Example usage
+  const el1 = await waitForElementDisplayed("xpath://android.widget.FrameLayout[@resource-id=\"club.partage.mobile.development:id/bottom_sheet\"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button/android.widget.ImageView");
+  await el1.click();
+  
+    //const el1 = await driver.$("xpath://android.widget.FrameLayout[@resource-id=\"club.partage.mobile.development:id/bottom_sheet\"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.Button/android.widget.ImageView");    
     console.log("reussi");
   } 
   catch (error) {
